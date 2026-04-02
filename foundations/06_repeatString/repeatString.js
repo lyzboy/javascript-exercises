db39b3b5bda79eec46671ee830161b6b6d40b10b
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-04-01 21:20:31 -0600

    Complete foundations lesson 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let returnString = ""
+  if (num < 0) return 'ERROR';
+  for (let i = 0; i < num; i++) {
+    returnString += string;
+  }
+  return returnString;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
