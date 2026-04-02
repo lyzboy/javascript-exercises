61369317decc12d96aeaa634b2eb2ed63dec0894
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-04-01 21:51:39 -0600

    complete foundations lesson 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  console.log(`arg[0]=${arguments[0]}`);
+  for (let i = 1; i < arguments.length; i++) {
+    arguments[0] = arguments[0].filter(elem => elem !== arguments[i]);
+    console.log(`after filter:${arguments[0]}`);
+  }
+  return arguments[0];
 };
 
 // Do not edit below this line
