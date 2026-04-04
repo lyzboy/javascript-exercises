392ea4a8e3d52ea0de7f859f47a7a976a28ec8fb
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-04-03 23:41:03 -0600

    Finish foundations lesson 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  // catch all restrictions
+  if (
+    a < 0 ||
+    b < 0 ||
+    !Number.isInteger(a) ||
+    !Number.isInteger(b)
+  ) return "ERROR";
+  // ensure are range is from lowest to highest number
+  return Array.from(
+    { length: Math.abs(Math.ceil((a - b))) + 1 },
+    (_, i) => a < b ? a + i : b + i,
+  ).reduce((acc, curr) => acc + curr, 0);
 };
 
 // Do not edit below this line
